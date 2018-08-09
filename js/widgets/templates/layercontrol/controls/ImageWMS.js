7eba18284466daaff2f9dea390939dbdf52c03d0
pri0ri7y <jai.priority@live.com>
2018-08-09 11:44:03 +0530

    commit for _control
    commit for _control

--- js/widgets/templates/layercontrol/controls/ImageWMS.js
+++ js/widgets/templates/layercontrol/controls/ImageWMS.js
@@ -3,7 +3,7 @@ define([
     'dijit/_WidgetBase',
     'dijit/_TemplatedMixin',
     'dijit/_Contained',
-    './_Control', // layer control base class
+    './control', // layer control base class
     './../plugins/legendWMSUtil'
 ], function (
     declare,
